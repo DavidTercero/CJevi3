20d100daf28595493f1679f4868cf225731ef12c
David Alejandro Tercero Caballero <datc0218@gmail.com>
2024-11-30 14:15:29 -0600

    Evidencia 3 CJ

---
+++ Evidencia3/src/Materia.java
@@ -0,0 +1,27 @@
+public class Materia {
+
+        private String nomMateria;
+        private double calificacion;
+
+        public Materia(String nomMateria, double calificacion) {
+            this.nomMateria = nomMateria;
+            this.calificacion = calificacion;
+        }
+
+        public String getNomMateria() {
+            return nomMateria;
+        }
+
+        public void setNomMateria(String nomMateria) {
+            this.nomMateria = nomMateria;
+        }
+
+        public double getCalificacion() {
+            return calificacion;
+        }
+
+        public void setCalificacion(double calificacion) {
+            this.calificacion = calificacion;
+        }
+    }
+
